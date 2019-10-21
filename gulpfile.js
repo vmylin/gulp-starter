3f185b034f88b8bb6307be633b1a5bbb1148eead
Victor Lin <vlinmy06@gmail.com>
2019-10-21 13:54:58 -0700

    change scripts to watch

--- gulpfile.js
+++ gulpfile.js
@@ -47,4 +47,4 @@ gulp.task("watch", function () {
     gulp.watch("js/*.js", gulp.series("scripts"));
 });
 
-gulp.task("default", gulp.parallel("browser-sync", "scripts")); 
+gulp.task("default", gulp.parallel("browser-sync", "watch")); 
